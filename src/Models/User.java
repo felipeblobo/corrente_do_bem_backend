bfd8b9a4b8a1a0c0a2d082589a485ef64d5bfafd
Felipe Lobo <felipeblobo@gmail.com>
2021-07-05 21:43:57 -0300

    commit inicial

---
+++ src/Models/User.java
@@ -0,0 +1,10 @@
+package Models;
+
+public abstract class User {
+    private int id;
+    private int type;
+    private String name;
+    private String phone;
+    private String email;
+    private String password;
+}
