726a254ae5f460170bd819532d22089461223e08
Raiane Honorato <raianehonorato16@gmail.com>
2021-08-14 14:47:02 -0300

    api documentation implantation + deploy of data base

--- src/main/java/br/com/correntedobembackend/correntedobembackend/controller/SubscriptionController.java
+++ src/main/java/br/com/correntedobembackend/correntedobembackend/controller/SubscriptionController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("subscription")
+@RequestMapping("/subscription")
 public class SubscriptionController {
 
     @Autowired
